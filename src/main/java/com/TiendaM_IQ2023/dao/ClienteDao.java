eae973a8a550b68c3accbbcf92285eb5f6b00a66
Bossaik <Bossaik@DESKTOP-DKBKB0Q>
2023-04-19 16:04:02 -0600

    Fix de la clase

--- src/main/java/com/TiendaM_IQ2023/dao/ClienteDao.java
+++ src/main/java/com/TiendaM_IQ2023/dao/ClienteDao.java
@@ -1,16 +1,12 @@
-
 package com.TiendaM_IQ2023.dao;
 
 import com.TiendaM_IQ2023.domain.Cliente;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
+public interface ClienteDao extends CrudRepository<Cliente, Long> {
 
-public interface ClienteDao extends CrudRepository<Cliente, Long>{
-    
+    public List<Cliente> findPorNombre(String nombre);
+
     //public List<Cliente> findByNombre(String nombre);
-    
-    public List<Cliente> findByApellidos(String apellidos);
-    
-    
 }
